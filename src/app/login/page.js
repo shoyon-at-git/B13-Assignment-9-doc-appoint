40c6d598547c6aee1f17472ac47518c5d6a24a18
shoyon-at-git <sanowarhossainshoyon123@gmail.com>
2026-05-23 12:42:31 +0600

    designed the login page

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,118 @@
-import React from 'react';
+"use client";
+
+import React, { useState } from "react";
+import Link from "next/link";
+import {
+    Button,
+    FieldError,
+    Form,
+    Input,
+    Label,
+    TextField,
+} from "@heroui/react";
+import { FaGoogle } from "react-icons/fa";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "react-toastify";
+import { redirect } from "next/navigation";
 
 const page = () => {
+    const [error,setError] = useState("");
+    const handleLogin=()=>{
+
+    }
+    const handleSocialSignup =()=>{
+
+    }
     return (
-        <div>
-            login
-        </div>
+        <div className="flex min-h-screen items-center justify-center bg-base-200 px-4">
+                    <div className="w-full max-w-md rounded-2xl bg-base-100 p-8 shadow-xl">
+                        <h1 className="mb-8 text-center text-4xl font-bold">
+                            Create new account
+                        </h1>
+        
+                        <Form onSubmit={handleLogin} className="flex flex-col gap-5">
+        
+                            <TextField
+                                isRequired
+                                name="email"
+                                type="email"
+                                validate={(value) => {
+                                    if (
+                                        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(
+                                            value
+                                        )
+                                    ) {
+                                        return "Please enter a valid email";
+                                    }
+                                    return null;
+                                }}
+                            >
+                                <Label>Email</Label>
+                                <Input placeholder="john@example.com" />
+                                <FieldError />
+                            </TextField>
+        
+                            <TextField
+                                isRequired
+                                name="password"
+                                type="password"
+                                validate={(value) => {
+                                    if (!value || value.length < 6) {
+                                        return "Password must be at least 6 characters";
+                                    }
+                                    if (!/[A-Z]/.test(value)) {
+                                        return "Must contain at least 1 uppercase letter";
+                                    }
+                                    if (!/[a-z]/.test(value)) {
+                                        return "Must contain at least 1 lowercase letter";
+                                    }
+                                    return null;
+                                }}
+                            >
+                                <Label>Password</Label>
+                                <Input placeholder="Enter your password" />
+                                <FieldError />
+                            </TextField>
+        
+                            {error && (
+                                <p className="text-sm font-medium text-red-500">
+                                    {error}
+                                </p>
+                            )}
+        
+                            <Button
+                                type="submit"
+                                className="w-full bg-black text-white"
+                            >
+                                Register
+                            </Button>
+                        </Form>
+        
+                        <div className="my-6 flex items-center gap-3">
+                            <div className="h-px flex-1 bg-gray-300"></div>
+                            <p className="text-sm text-gray-500">OR</p>
+                            <div className="h-px flex-1 bg-gray-300"></div>
+                        </div>
+        
+                        <Button
+                            onClick={handleSocialSignup}
+                            className="flex w-full items-center justify-center gap-3 rounded-xl bg-white py-3 font-medium text-gray-800 shadow-md transition-all duration-300 hover:-translate-y-0.5 hover:shadow-xl active:translate-y-0 border border-gray-200"
+                        >
+                            <FaGoogle className="text-xl text-red-500" />
+                            <span>Continue with Google</span>
+                        </Button>
+        
+                        <p className="mt-6 text-center text-sm">
+                            Already have an account?{" "}
+                            <Link
+                                href="/login"
+                                className="font-semibold text-blue-600 hover:underline"
+                            >
+                                Login
+                            </Link>
+                        </p>
+                    </div>
+                </div>
     );
 };
 
